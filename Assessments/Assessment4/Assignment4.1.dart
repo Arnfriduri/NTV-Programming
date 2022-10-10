58dcf545da92f82f7fb91334058257d2630a871a
Arnfriduri <32144594+Arnfriduri@users.noreply.github.com>
2022-10-10 00:26:19 +0000

    Made the files for assignment 4

--- Assessments/Assessment4/Assignment4.1.dart
+++ Assessments/Assessment4/Assignment4.1.dart
@@ -8,25 +8,7 @@ import 'dart:convert';
 import 'dart:io';
 
 void main() {
-  List<int> calculate_sum = [
-    13,
-    56,
-    77,
-    3,
-    45,
-    6,
-    944,
-    1,
-    43,
-    7,
-    40,
-    55,
-    67,
-    93,
-    45,
-    754,
-    33
-  ];
+  List<int> calculate_sum = [13,56,77,3,45,6,944,1,43,7,40,55,67,93,45,754,33];
   int sum = 0;
 
   for (var number in calculate_sum) {
