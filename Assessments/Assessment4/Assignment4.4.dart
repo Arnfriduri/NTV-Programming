10c8757e086ee4a2fc7fafa997cebdf212ecb96f
Arnfriduri <32144594+Arnfriduri@users.noreply.github.com>
2022-10-10 19:11:48 +0000

    Finished the third one

--- Assessments/Assessment4/Assignment4.4.dart
+++ Assessments/Assessment4/Assignment4.4.dart
@@ -9,4 +9,23 @@ Program written by: Arnfríður Ingvarsdóttir
 import 'dart:convert';
 import 'dart:io';
 
-void main() {}
+void main() {
+  List strings = [];
+  bool stop = true;
+
+  while(stop == true){
+    print('Please enter a string: ');
+    String stringEntered = stdin.readLineSync(encoding: utf8);
+    //checking if a string was entered
+    if(stringEntered.isEmpty){
+      stop = false;
+    }
+    //placing the string entered into our list
+    else {
+      strings.add(stringEntered);
+    }
+  }
+  print(strings);
+  var reversed = strings.reversed;
+  print(reversed.toList());
+}
