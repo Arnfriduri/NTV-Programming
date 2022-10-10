cf02a122fdb0dae9ebef1f09855170e16296590e
Arnfriduri <32144594+Arnfriduri@users.noreply.github.com>
2022-10-10 06:02:23 +0000

    Assignment 2 done

--- Assessments/Assessment4/Assignment4.2.dart
+++ Assessments/Assessment4/Assignment4.2.dart
@@ -8,4 +8,16 @@ Program written by: Arnfríður Ingvarsdóttir
 import 'dart:convert';
 import 'dart:io';
 
-void main() {}
+void main() {
+
+  int digit = 0;
+  List<int> calculate_sum = [13,56,77,3,45,6,944,1,43,7,40,55,67,93,45,754,33];
+
+  for(var i = 0; i < calculate_sum.length; i++){
+    if(calculate_sum[i] >= 20 && calculate_sum[i] <= 80){
+      print(calculate_sum[i]);
+      digit +=1;
+    }
+  }
+  print(digit);
+}
